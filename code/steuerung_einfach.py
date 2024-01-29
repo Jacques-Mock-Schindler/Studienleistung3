f1afcfbd6e10fe8918aa2e582ab333cec6739660
Jacques Mock Schindler <jacques.mock@kbw.ch>
2024-01-29 16:04:11 +0100

    Kommentiere die Funktion drehung

--- code/steuerung_einfach.py
+++ code/steuerung_einfach.py
@@ -10,13 +10,22 @@ def ausfahrt():
     der Roboterstaubsauger nicht bei jeder Ausfahrt an der selben Stelle
     ablenkt.
     """
-    
+
     time = random.randrange(500,2000,500)
     forward()
     delay(time)
     stop()
 
 def drehung():
+    """
+    Die Funktion steuert die Ablenkungen nach der Ausfahrt oder um einem
+    Hindernis auszuweichen. Der Abrehwinkel wird dabei zufällig
+    festgelegt.  
+    Mit time = random.randrange(200,1100,50) wird der Winkel zwischen ca
+    30 und 90 Grad festgelegt.
+    Mit direction = random.choice([1,-1]) wird die Drehrichtung zufällig
+    auf links und rechts verteilt.
+    """
     time = random.randrange(200,1100,50)
     direction = random.choice([1,-1])
     if direction == 1:
